cf312c5aed782ccacd147b6b0b4d7c0405a8ea2e
Gulkin Igor <ardar.igor@mail.ru>
2017-11-04 20:14:35 +0300

    Разработано бонусов 24/25

--- src/Adjutants/AdjutantFielder.java
+++ src/Adjutants/AdjutantFielder.java
@@ -47,59 +47,59 @@ public class AdjutantFielder {
         }
     }
 
-    private List<ZoneOfPlayer> searchZones(BattleManager battleManager){
-        List<ZoneOfPlayer> listOfZones = new ArrayList<>();
-        currentZoneOfPlayer = new ZoneOfPlayer(new ArrayList<>(), 0); //Текущая область
-        listPassed = new ArrayList<>(); //Лист пройденных точек
-
-        for (int i = 0; i < 16; i++){
-            for (int j = 0; j < 16; j++){
-                String unity = battleManager.getBattleField().getMatrix().get(i).get(j);
-                Point cursorPoint = new Point(i, j);
-                if (unity.equals("     0") && !listPassed.contains(cursorPoint)){
-                    currentZoneOfPlayer = new ZoneOfPlayer(new ArrayList<>(), 0);
-                    currentZoneOfPlayer.getArea().add(cursorPoint);
-                    listPassed.add(cursorPoint);
-                        step(battleManager, cursorPoint);
-                    listOfZones.add(currentZoneOfPlayer);
-                }
-            }
-        }
-        return listOfZones;
-    }
-
-    private void step(BattleManager battleManager, Point cursorPoint){
-        boolean isNotFullZone = true; //Зона не полностью заполнена
-        while (isNotFullZone){
-            List<Point> closestPoints = new ArrayList<>();
-            Point pointUp = new Point(cursorPoint.X() + 1, cursorPoint.Y());
-            Point pointUpRight = new Point(cursorPoint.X() + 1, cursorPoint.Y() + 1);
-            Point pointUpLeft = new Point(cursorPoint.X() + 1, cursorPoint.Y() - 1);
-            Point pointDown = new Point(cursorPoint.X() - 1, cursorPoint.Y());
-            Point pointDownRight = new Point(cursorPoint.X() - 1, cursorPoint.Y() + 1);
-            Point pointDownLeft = new Point(cursorPoint.X() - 1, cursorPoint.Y() - 1);
-            Point pointLeft = new Point(cursorPoint.X(), cursorPoint.Y() - 1);
-            Point pointRight = new Point(cursorPoint.X(), cursorPoint.Y() + 1);
-
-            checkRoad(battleManager, pointUp, closestPoints);
-            checkRoad(battleManager, pointUpRight, closestPoints);
-            checkRoad(battleManager, pointRight, closestPoints);
-            checkRoad(battleManager, pointDownRight, closestPoints);
-            checkRoad(battleManager, pointDown, closestPoints);
-            checkRoad(battleManager, pointDownLeft, closestPoints);
-            checkRoad(battleManager, pointLeft, closestPoints);
-            checkRoad(battleManager, pointUpLeft, closestPoints);
-            if (closestPoints.size() == 0){
-                isNotFullZone = false;
-            } else {
-                for (int i = 0; i < closestPoints.size() - 1; i++){
-                    currentZoneOfPlayer.getArea().add(closestPoints.get(i));
-                    listPassed.add(closestPoints.get(i));
-                    step(battleManager, closestPoints.get(i));
-                }
-            }
-        }
-    }
+//    private List<ZoneOfPlayer> searchZones(BattleManager battleManager){
+//        List<ZoneOfPlayer> listOfZones = new ArrayList<>();
+//        currentZoneOfPlayer = new ZoneOfPlayer(new ArrayList<>(), 0); //Текущая область
+//        listPassed = new ArrayList<>(); //Лист пройденных точек
+//
+//        for (int i = 0; i < 16; i++){
+//            for (int j = 0; j < 16; j++){
+//                String unity = battleManager.getBattleField().getMatrix().get(i).get(j);
+//                Point cursorPoint = new Point(i, j);
+//                if (unity.equals("     0") && !listPassed.contains(cursorPoint)){
+//                    currentZoneOfPlayer = new ZoneOfPlayer(new ArrayList<>(), 0);
+//                    currentZoneOfPlayer.getArea().add(cursorPoint);
+//                    listPassed.add(cursorPoint);
+//                        step(battleManager, cursorPoint);
+//                    listOfZones.add(currentZoneOfPlayer);
+//                }
+//            }
+//        }
+//        return listOfZones;
+//    }
+//
+//    private void step(BattleManager battleManager, Point cursorPoint){
+//        boolean isNotFullZone = true; //Зона не полностью заполнена
+//        while (isNotFullZone){
+//            List<Point> closestPoints = new ArrayList<>();
+//            Point pointUp = new Point(cursorPoint.X() + 1, cursorPoint.Y());
+//            Point pointUpRight = new Point(cursorPoint.X() + 1, cursorPoint.Y() + 1);
+//            Point pointUpLeft = new Point(cursorPoint.X() + 1, cursorPoint.Y() - 1);
+//            Point pointDown = new Point(cursorPoint.X() - 1, cursorPoint.Y());
+//            Point pointDownRight = new Point(cursorPoint.X() - 1, cursorPoint.Y() + 1);
+//            Point pointDownLeft = new Point(cursorPoint.X() - 1, cursorPoint.Y() - 1);
+//            Point pointLeft = new Point(cursorPoint.X(), cursorPoint.Y() - 1);
+//            Point pointRight = new Point(cursorPoint.X(), cursorPoint.Y() + 1);
+//
+//            checkRoad(battleManager, pointUp, closestPoints);
+//            checkRoad(battleManager, pointUpRight, closestPoints);
+//            checkRoad(battleManager, pointRight, closestPoints);
+//            checkRoad(battleManager, pointDownRight, closestPoints);
+//            checkRoad(battleManager, pointDown, closestPoints);
+//            checkRoad(battleManager, pointDownLeft, closestPoints);
+//            checkRoad(battleManager, pointLeft, closestPoints);
+//            checkRoad(battleManager, pointUpLeft, closestPoints);
+//            if (closestPoints.size() == 0){
+//                isNotFullZone = false;
+//            } else {
+//                for (int i = 0; i < closestPoints.size() - 1; i++){
+//                    currentZoneOfPlayer.getArea().add(closestPoints.get(i));
+//                    listPassed.add(closestPoints.get(i));
+//                    step(battleManager, closestPoints.get(i));
+//                }
+//            }
+//        }
+//    }
 
     private void checkRoad(BattleManager battleManager, Point point, List<Point> closestPoints){
         boolean inBounds = point.X() >= 0 && point.X() < 16 && point.Y() >= 0 && point.Y() < 16;
