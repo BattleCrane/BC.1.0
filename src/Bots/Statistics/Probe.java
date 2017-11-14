c413b80233bbb0abe903d6ae562dd8898bb47a41
Gulkin Igor <ardar.igor@mail.ru>
2017-11-14 23:40:29 +0300

    В разработке метод ослеживания безопасных областей

--- src/Bots/Statistics/Probe.java
+++ src/Bots/Statistics/Probe.java
@@ -2,126 +2,91 @@ package Bots.Statistics;
 
 import BattleFields.BattleManager;
 import BattleFields.Point;
+import Players.Player;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import static Adjutants.AdjutantAttacker.max;
-import static Adjutants.AdjutantAttacker.min;
-
 public interface Probe {
-    default List<Point> probeSafeZone(BattleManager battleManager){
-        List<Point> listSafeZone = new ArrayList<>();
+
+    default List<Point> probeDangerousZone(BattleManager battleManager) {
+        List<Point> listDangerousZone = new ArrayList<>();
         List<List<String>> matrix = battleManager.getBattleField().getMatrix();
-        Pattern patternTurrets = Pattern.compile("[tu]");
         Pattern pattern = Pattern.compile("[GT]");
         Pattern patternBonus = Pattern.compile("[HCBEiQ]");
-        for (int i = 0;  i < 16; i++){
-            for (int j = 0; j < 16; j++){
+        Player currentPlayer = battleManager.getPlayer();
+        for (int i = 0; i < 16; i++) {
+            for (int j = 0; j < 16; j++) {
                 String current = matrix.get(j).get(i);
-                if (current.substring(3, 4).equals(battleManager.getOpponentPlayer().getColorType())){
+                if (current.substring(3, 4).equals(battleManager.getOpponentPlayer().getColorType())) { //Если это точка противника
                     Matcher matcher = pattern.matcher(current);
                     Matcher matcherBonus = patternBonus.matcher(current);
-                    Matcher matcherTurrets = patternTurrets.matcher(current);
-                    if (matcherBonus.find() || matcher.find()){
-
-
-
-
+                    if (matcherBonus.find() || matcher.find()) { //Если это вражеский юнит, кторый стреляет по прямым и диагоналям:
+                        shift(currentPlayer, matrix, listDangerousZone, -1, 0, new Point(j, i));
+                        shift(currentPlayer, matrix, listDangerousZone, 0, -1, new Point(j, i));
+                        shift(currentPlayer, matrix, listDangerousZone, 1, 0, new Point(j, i));
+                        shift(currentPlayer, matrix, listDangerousZone, 0, 1, new Point(j, i));
+                        shift(currentPlayer, matrix, listDangerousZone, -1, -1, new Point(j, i));
+                        shift(currentPlayer, matrix, listDangerousZone, -1, 1, new Point(j, i));
+                        shift(currentPlayer, matrix, listDangerousZone, 1, -1, new Point(j, i));
+                        shift(currentPlayer, matrix, listDangerousZone, 1, 1, new Point(j, i));
                     }
-                    if ( matcherTurrets.find()){
 
+                    //Если это вражеская турель:
+                    int radius = 0;
+                    switch (current.substring(1, 2) + current.substring(4, 5)) {
+                        case "^t":
+                            radius = 2;
+                            break;
+                        case "<t":
+                            radius = 3;
+                            break;
+                        case "^u":
+                        case "<u":
+                            radius = 5;
                     }
+                    radiusMark(matrix, listDangerousZone, radius, new Point(j, i));
                 }
-
             }
         }
-
-
-        return listSafeZone;
+        return listDangerousZone;
     }
 
-    private void shift(List<List<String>> matrix, List<Point> listSafeZone, int dx, int dy, Point start){
-        if (matrix.get(start.Y() + dy).get(start.X() + dx).substring(1).equals("    0")){
+    //Определение опасных точек от автоматчиков, танков
+    private void shift(Player currentPlayer, List<List<String>> matrix, List<Point> listDangerousZone, int dx, int dy, Point start) {
+        Pattern patternBuildings = Pattern.compile("[hgbfwt]");
+        String currentUnity = matrix.get(start.Y() + dy).get(start.X() + dx);
+        Matcher matcher = patternBuildings.matcher(currentUnity);
+        if (!matcher.find() && !currentUnity.substring(3, 4).equals(currentPlayer.getColorType())) {
             Point nextPoint = new Point(start.Y() + dy, start.X() + dx);
-            listSafeZone.add(nextPoint);
-            shift(matrix, listSafeZone, dx, dy, nextPoint);
+            if (!listDangerousZone.contains(nextPoint)) {
+                listDangerousZone.add(nextPoint);
+            }
+            shift(currentPlayer, matrix, listDangerousZone, dx, dy, nextPoint);
         }
     }
 
-//    private boolean markDangerous(BattleManager battleManager, Point attackerPoint) {
-//        while ()
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//        if (attackerPoint.X() == targetPoint.X()) {
-//            for (int j = min(attackerPoint.Y(), targetPoint.Y()) + 1; j < max(attackerPoint.Y(), targetPoint.Y()); j++) {
-//                String currentUnity = battleManager.getBattleField().getMatrix().get(attackerPoint.X()).get(j);
-//                Matcher matcher = pattern.matcher(currentUnity);
-//                Matcher matcherBonuses = patternBonuses.matcher(currentUnity);
-//                if ((matcher.find() || matcherBonuses.find()) && !currentUnity.contains(battleManager.getPlayer().getColorType())) {
-//                    return false;
-//                }
-//            }
-//            return true;
-//        }
-//        if (attackerPoint.Y() == targetPoint.Y()) {
-//            for (int i = min(attackerPoint.X(), targetPoint.X()) + 1; i < max(attackerPoint.X(), targetPoint.X()); i++) {
-//                String currentUnity = battleManager.getBattleField().getMatrix().get(i).get(attackerPoint.Y());
-//                Matcher matcher = pattern.matcher(currentUnity);
-//                Matcher matcherBonuses = patternBonuses.matcher(currentUnity);
-//                if ((matcher.find() || matcherBonuses.find()) && !currentUnity.contains(battleManager.getPlayer().getColorType())) {
-//                    return false;
-//                }
-//            }
-//            return true;
-//        }
-//        if (Math.abs(attackerPoint.X() - targetPoint.X()) == Math.abs(attackerPoint.Y() - targetPoint.Y())) { //Если лежат на одной диагонали
-//            final boolean distanceY = targetPoint.Y() - attackerPoint.Y() > 0;
-//            final boolean distanceX = targetPoint.X() - attackerPoint.X() > 0;
-//            int pointerX = attackerPoint.X();
-//            int pointerY = attackerPoint.Y();
-//            int count  = 0;
-//            while (pointerX != targetPoint.X() && pointerY != targetPoint.Y()) {
-//                if (distanceY && distanceX) {
-//                    pointerX++;
-//                    pointerY++;
-//                }
-//                if (distanceY && !distanceX) {
-//                    pointerX--;
-//                    pointerY++;
-//                }
-//                if (!distanceY && distanceX) {
-//                    pointerX++;
-//                    pointerY--;
-//                }
-//                if (!distanceX && !distanceY){
-//                    pointerX--;
-//                    pointerY--;
-//                }
-//                count++;
-//                String currentUnity = battleManager.getBattleField().getMatrix().get(pointerX).get(pointerY);
-//                Matcher matcher = pattern.matcher(currentUnity);
-//                Matcher matcherBonuses = patternBonuses.matcher(currentUnity);
-//                if (count != 0 && count != Math.abs(targetPoint.X() - attackerPoint.X()) && (matcher.find() || matcherBonuses.find()) &&
-//                        !currentUnity.contains(battleManager.getPlayer().getColorType())){
-//                    return false;
-//                }
-//            }
-//            return true;
-//        }
-//        return false;
-//    }
-//
+
+    //Определение опасных точек от турелей:
+    private void radiusMark(List<List<String>> matrix, List<Point> listDangerousZone, int radius, Point middle) {
+        int x = middle.X();
+        int y = middle.Y();
+        int countShift = 0; //"Пирамидальный сдвиг": с каждой итерируется по горизонтали с формулой 2i -1
+        for (int i = x - radius; i < x + radius + 1; i++) {
+            for (int j = y - countShift; j < y + 1 + countShift; j++) {
+                boolean inBounds = i >= 0 && i < 16 && j >= 0 && j < 16;
+                if (inBounds && listDangerousZone.contains(new Point(j, i))) {
+                    listDangerousZone.add(new Point(j, i));
+                }
+            }
+            countShift++;
+            if (i >= x) {
+                countShift = countShift - 2; //Перетягивание countShift--
+            }
+        }
+    }
+
 
 }
