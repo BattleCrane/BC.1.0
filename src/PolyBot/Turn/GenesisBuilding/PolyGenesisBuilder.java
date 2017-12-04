8d172c6c652aeba8d38c70ab1886f85d61ba6916
StarKRE <ardar.igor@mail.ru>
2017-12-04 22:25:53 +0300

    Разработка генетического алгоритма для постройки зданий. Этап II. Скрещивание.

--- src/PolyBot/Turn/GenesisBuilding/PolyGenesisBuilder.java
+++ src/PolyBot/Turn/GenesisBuilding/PolyGenesisBuilder.java
@@ -7,10 +7,16 @@ import PolyBot.Priority.PolyPriorityUnit;
 import PolyBot.Turn.Creating.CreatingList;
 import PolyBot.Turn.Creating.ConditionalUnit;
 import PolyBot.Turn.PolyProbe;
+import Unities.Unity;
 
 import java.util.*;
 
 public class PolyGenesisBuilder {
+    PolyGenesisBuilder (){}
+
+    PolyGenesisBuilder(BattleManager battleManager){
+
+    }
 
 
     private Set<CreatingList> combinations = new HashSet<>();
@@ -139,6 +145,104 @@ public class PolyGenesisBuilder {
                 combinations.add(mergeCombination);
             }
         }
+    }
+
+    private CreatingList mutate(BattleManager battleManager, int howICanBuild, CreatingList creatingList){
+        CreatingList mutatedCombination = new CreatingList(new ArrayList<>(), 0);
+        Map<Unity, ConditionalUnit> conditionalUnitMap = Map.of(
+                //Barracks:
+                battleManager.getBarracks(), new ConditionalUnit(battleManager, battleManager.getBarracks(), (s) -> {
+                }, (e) -> {
+                }) {
+                    @Override
+                    public boolean isPerformedCondition(Point point) {
+                        return battleManager.isEmptyTerritory(point, battleManager.getBarracks()) &&
+                                battleManager.canConstructBuilding(point, battleManager.getBarracks(), battleManager.getPlayer());
+                    }
+                },
+                //Factory:
+                battleManager.getBarracks(), new ConditionalUnit(battleManager, battleManager.getFactory(),
+                        (s) -> battleManager.setHowICanBuildFactories(battleManager.getHowCanBuildFactories() - 1),
+                        (e) -> battleManager.setHowICanBuildFactories(battleManager.getHowCanBuildFactories() + 1)) {
+                    @Override
+                    public boolean isPerformedCondition(Point point) {
+                        return battleManager.getHowCanBuildFactories() > 0 && battleManager.canConstructBuilding(point,
+                                battleManager.getFactory(), battleManager.getPlayer()) &&
+                                battleManager.isEmptyTerritory(point, battleManager.getFactory());
+                    }
+                },
+                //Generator:
+                battleManager.getBarracks(), new ConditionalUnit(battleManager, battleManager.getGenerator(),
+                        (s) -> battleManager.setConstructedGenerator(true),
+                        (e) -> battleManager.setConstructedGenerator(false)) {
+                    @Override
+                    public boolean isPerformedCondition(Point point) {
+                        return howICanBuild <= 2 && !battleManager.isConstructedGenerator() &&
+                                battleManager.canConstructBuilding(point, battleManager.getGenerator(), battleManager.getPlayer()) &&
+                                battleManager.isEmptyTerritory(point, battleManager.getGenerator());
+                    }
+                }
+        );
+
+        for (PriorityUnit priorityUnit: creatingList.getPriorityUnitList()){ //Идем по всем расположенным юнитам:
+            ConditionalUnit conditionalUnit = conditionalUnitMap.get(priorityUnit.getUnity());
+            if (!conditionalUnit.isPerformedCondition(priorityUnit.getPoint())){ //Если условия не выполняются
+
+            }
+
+
+
+
+
+            for (int j = 0; j < 16; j++) {
+                String currentUnity = battleManager.getBattleField().getMatrix().get(i).get(j);
+                Point point = new Point(j, i);
+                if (currentUnity.substring(1).equals("    0")) { //Если пустая клетка
+
+                    if (conditionalUnit.isPerformedCondition(point)) {//Если территория свободна и рядом есть мои строения
+                        PriorityUnit priorityUnit = new PolyProbe().probeBuildingTest(battleManager, conditionalUnit.getUnity(), point); //Исследуем приоритет на бараки
+                        if (!currentCombinationOfBuild.contains(priorityUnit) && battleManager.putUnity(battleManager.getPlayer(), point, priorityUnit.getUnity())) { //Если нет в текущем списке и построилось строение
+                            currentCombinationOfBuild.add(priorityUnit);
+                            int nextBuild = howICanBuild - 1;
+                            conditionalUnit.getStartPredicate().run(battleManager);
+                            if (nextBuild > 0) {
+                                findCombination(battleManager, nextBuild);
+                            } else {
+                                if (currentCombinationOfBuild.getSum() > bestCombinationOfBuild.getSum()) {
+                                    max = currentCombinationOfBuild.getSum();
+                                    bestCombinationOfBuild = new CreatingList(new ArrayList<>(), 0);
+                                    for (PriorityUnit p : currentCombinationOfBuild.getPriorityUnitList()) {
+                                        bestCombinationOfBuild.add(new PolyPriorityUnit(p.getPriority(), p.getPoint(), p.getUnity()));
+                                    }
+                                }
+                            }
+                            currentCombinationOfBuild.removeLast();
+                            battleManager.removeUnity(point, conditionalUnit.getUnity(), currentUnity.substring(0, 1));
+                            conditionalUnit.getEndPredicate().run(battleManager);
+//                            new AdjutantFielder().flush(battleManager);
+//                            new AdjutantFielder().fillZones(battleManager);
+                        }
+                    }
+
+
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+        for (int i = 0; i < 16; i++) {
+        return mutatedCombination;
     }
 
     public CreatingList getBestCombinationOfBuild() {
